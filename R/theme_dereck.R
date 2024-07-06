fba0e7a8e58a735233263a489f31fd47f76c095e
dereckmezquita <dereck@mezquita.io>
2024-07-06 15:23:39 -0500

    Precommit not working.

--- R/theme_dereck.R
+++ R/theme_dereck.R
@@ -10,7 +10,7 @@
 #'     ggplot2::ggplot(ggplot2::aes(x = Sepal.Length, y = Sepal.Width)) +
 #'     ggplot2::geom_point(ggplot2::aes(color = Species, shape = Species)) +
 #'     ggplot2::labs(title = "Iris data", subtitle = "Classic data", x = "Sepal Length", y = "Sepal Width") +
-#'     ggplot2::facet_grid(~ Species) +
+#'     ggplot2::facet_grid(~Species) +
 #'     dmplot::theme_dereck_light()
 #'
 #' data.frame(year = time(nhtemp), tmps = as.matrix(nhtemp)) |>
@@ -24,20 +24,16 @@ theme_dereck_light <- function(base_size = 11, base_family = "") {
     ggplot2::theme_bw(base_size = base_size, base_family = base_family) %+replace% ggplot2::theme(
         plot.title = ggplot2::element_text(face = "bold", hjust = 1, margin = ggplot2::margin(0, 0, 5, 0, unit = "pt")), # 0.5 centres when position is set to "plot"
         plot.subtitle = ggplot2::element_text(hjust = 1, vjust = 0.015, margin = ggplot2::margin(0, 0, 5, 0, unit = "pt")),
-
         panel.background = ggplot2::element_blank(),
         # panel.border = ggplot2::element_rect(color = "#afafaf", fill = NA),
         axis.line = ggplot2::element_blank(),
         panel.border = ggplot2::element_blank(),
-
         panel.grid.minor = ggplot2::element_line(color = "#454545", size = 0.135, linetype = "dotted"),
         panel.grid.major = ggplot2::element_line(color = "#aaaaaa", size = 0.175, linetype = "longdash"),
 
         # axis.line = ggplot2::element_line(color = "#afafaf"),
         axis.ticks = ggplot2::element_line(color = "#aaaaaa"),
-
         strip.background = ggplot2::element_rect(fill = "white", colour = "#676767"),
-
         legend.position = "bottom",
         legend.title = ggplot2::element_text(vjust = 0.85) # adjust title position higher to key when bottom
     )
@@ -55,7 +51,7 @@ theme_dereck_light <- function(base_size = 11, base_family = "") {
 #'     ggplot2::ggplot(ggplot2::aes(x = Sepal.Length, y = Sepal.Width)) +
 #'     ggplot2::geom_point(ggplot2::aes(color = Species, shape = Species)) +
 #'     ggplot2::labs(title = "Iris data", subtitle = "Classic data", x = "Sepal Length", y = "Sepal Width") +
-#'     ggplot2::facet_grid(~ Species) +
+#'     ggplot2::facet_grid(~Species) +
 #'     dmplot::theme_dereck_dark()
 #'
 #' data.frame(year = time(nhtemp), tmps = as.matrix(nhtemp)) |>
@@ -74,22 +70,19 @@ theme_dereck_dark <- function(base_size = 12, base_family = "") {
         # panel.border = ggplot2::element_rect(color = "#afafaf", fill = NA),
         axis.line = ggplot2::element_blank(),
         panel.border = ggplot2::element_blank(),
-
         panel.grid.minor = ggplot2::element_line(color = "white", size = 0.135, linetype = "dotted"),
         panel.grid.major = ggplot2::element_line(color = "#aaaaaa", size = 0.175, linetype = "longdash"),
 
         # axis.line = ggplot2::element_line(color = "#afafaf"),
         axis.ticks = ggplot2::element_line(color = "#aaaaaa"),
-
         strip.background = ggplot2::element_rect(fill = "white", colour = "#676767"),
-
         legend.position = "bottom",
         legend.title = ggplot2::element_text(vjust = 0.85), # adjust title position higher to key when bottom
 
         # dark theme configs
         plot.background = ggplot2::element_rect(fill = "#333333"),
         panel.background = ggplot2::element_rect(fill = "#1E1E1E"),
-        legend.background = ggplot2::element_rect(fill = "#252526"), # 
+        legend.background = ggplot2::element_rect(fill = "#252526"), #
         text = ggplot2::element_text(colour = "#CFCFCF"),
         axis.text = ggplot2::element_text(colour = "#CFCFCF")
     )
