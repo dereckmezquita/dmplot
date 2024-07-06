235361bfd0c1292b4bdc7e72be4452bf97180631
dereckmezquita <dereck@mezquita.io>
2024-07-06 13:45:50 -0500

    Testing.

--- dev/candlesticks/index.R
+++ dev/candlesticks/index.R
@@ -1,5 +1,17 @@
 dt <- data.table::fread("./demo/data/bitcoin-hourly-data.csv")
 
+lines <- data.table::data.table(
+    x = dt$datetime,
+    y = dt$close,
+    group = 1
+)
+
+lines <- rbind(lines, data.table::data.table(
+    x = dt$datetime,
+    y = dt$high,
+    group = 2
+))
+
 dt |>
     ggplot2::ggplot() +
     stat_candlestick(ggplot2::aes(
@@ -9,4 +21,10 @@ dt |>
         high = high,
         low = low,
         group = symbol
+    )) +
+    ggplot2::geom_line(data = lines, ggplot2::aes(
+        x = x,
+        y = y,
+        group = group,
+        colour = as.character(group)
     ))
